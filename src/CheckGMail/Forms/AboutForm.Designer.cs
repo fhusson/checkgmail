3e7184d8509afebb7b1bb21c5454b9dfa37c2bff
whaly <whaly@baleinoid.fr>
2017-12-10 12:37:37 +0100

    Update About txt and Chagelog

--- src/CheckGMail/Forms/AboutForm.Designer.cs
+++ src/CheckGMail/Forms/AboutForm.Designer.cs
@@ -33,14 +33,15 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.labelProduct = new System.Windows.Forms.Label();
             this.labelVersion = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonOk
             // 
-            this.buttonOk.Location = new System.Drawing.Point(51, 172);
+            this.buttonOk.Location = new System.Drawing.Point(16, 294);
             this.buttonOk.Name = "buttonOk";
-            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.Size = new System.Drawing.Size(394, 23);
             this.buttonOk.TabIndex = 0;
             this.buttonOk.Text = "&OK";
             this.buttonOk.UseVisualStyleBackColor = true;
@@ -49,9 +50,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(24, 33);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 34);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.Size = new System.Drawing.Size(394, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
@@ -59,7 +61,7 @@
             // 
             this.labelProduct.AutoSize = true;
             this.labelProduct.Font = new System.Drawing.Font("Lucida Console", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelProduct.Location = new System.Drawing.Point(13, 9);
+            this.labelProduct.Location = new System.Drawing.Point(12, 9);
             this.labelProduct.Name = "labelProduct";
             this.labelProduct.Size = new System.Drawing.Size(150, 22);
             this.labelProduct.TabIndex = 2;
@@ -69,18 +71,27 @@
             // 
             this.labelVersion.AutoSize = true;
             this.labelVersion.Font = new System.Drawing.Font("Lucida Console", 8.25F);
-            this.labelVersion.Location = new System.Drawing.Point(44, 33);
+            this.labelVersion.Location = new System.Drawing.Point(328, 9);
             this.labelVersion.Name = "labelVersion";
             this.labelVersion.Size = new System.Drawing.Size(82, 11);
             this.labelVersion.TabIndex = 3;
             this.labelVersion.Text = "Version x.y";
             this.labelVersion.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 166);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(394, 122);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(175, 205);
+            this.ClientSize = new System.Drawing.Size(424, 322);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.labelVersion);
             this.Controls.Add(this.labelProduct);
             this.Controls.Add(this.pictureBox1);
@@ -104,5 +115,6 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label labelProduct;
         private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
